9949015b49fa7c28e4e9c6f1b3e9564ce8723263
PresentKim <debe3721@gmail.com>
2021-03-28 00:04:21 +0900

    Load css file via css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
+const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        index: "./src/index.js",
+        styles: "./src/styles/index.js",
+    },
     output: {
         path: __dirname,
-        filename: "./dist/build.js",
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js",
     },
     module: {
         rules: [
             {
-                test: /\.(js)$/,
+                test: /\.(js)$/i,
                 exclude: /node_modules/,
                 use: ["babel-loader"]
+            },
+            {
+                test: /\.(css)$/i,
+                use: ["style-loader", "css-loader"],
             }
         ]
     }
