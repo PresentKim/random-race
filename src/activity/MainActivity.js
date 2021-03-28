18c18f694bcde3d7e2a93a334ba035b16639ac0d
PresentKim <debe3721@gmail.com>
2021-03-29 01:43:00 +0900

    Define button icon sprite

--- src/activity/MainActivity.js
+++ src/activity/MainActivity.js
@@ -1,12 +1,11 @@
 import Activity from "./Activity";
-import {BackgroundSpriteSheet} from "@/sprite/SpriteSheetDefs";
+import {BackgroundSpriteSheet, IconSpriteSheet} from "@/sprite/SpriteSheetDefs";
 import RenderOption from "@/utils/RenderOption";
 import Vector2 from "@/utils/Vector2";
 import TextWidget from "@/widget/TextWidget";
 import BackgroundWidget from "@/widget/BackgroundWidget";
 import SpriteWidget from "@/widget/SpriteWidget";
 import Sprite from "@/sprite/Sprite";
-import ReloadPng from "~/ui/button/restart.png";
 
 class MainActivity extends Activity {
     /** @param {App} app */
@@ -17,8 +16,8 @@ class MainActivity extends Activity {
         background.onUpdate = (diffSecs) => {
             background.pos.x -= diffSecs / 10;
         };
-        const titleText = new TextWidget(Vector2.from(app).multiply(0.5, 0.05), "random race", RenderOption.absolute().scale(5));
-        const reloadButton = new SpriteWidget(Vector2.from(app).multiply(0.95, 0.05), new Sprite(21, 22).setImage(ReloadPng), RenderOption.absolute().scale(2));
+        const titleText = new TextWidget(Vector2.from(app).multiply(0.5, 0.1), "random race", RenderOption.absolute().scale(5));
+        const reloadButton = new SpriteWidget(Vector2.from(app).multiply(0.95, 0.1), IconSpriteSheet.get("reset"), RenderOption.absolute().scale(3));
         reloadButton.onClick = () => {
             background.sprite = BackgroundSpriteSheet.random();
             return true;
--- src/sprite/SpriteSheetDefs.js
+++ src/sprite/SpriteSheetDefs.js
@@ -3,6 +3,9 @@ import TextPng from "~/ui/text.png";
 import TextJson from "~/ui/text.json";
 import BackgroundPng from "~/ui/background.png";
 import BackgroundJson from "~/ui/background.json";
+import IconPng from "~/ui/icon.png";
+import IconJson from "~/ui/icon.json";
 
 export const TextSpriteSheet = new SpriteSheet(TextPng, TextJson);
 export const BackgroundSpriteSheet = new SpriteSheet(BackgroundPng, BackgroundJson);
+export const IconSpriteSheet = new SpriteSheet(IconPng, IconJson);
