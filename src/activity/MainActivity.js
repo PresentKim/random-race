bc245eb340bc7b74856689aa3933473b3e215a7d
PresentKim <debe3721@gmail.com>
2021-03-28 22:44:47 +0900

    MainActivity : Add reload button for relaod background

--- src/activity/MainActivity.js
+++ src/activity/MainActivity.js
@@ -4,6 +4,9 @@ import RenderOption from "@/utils/RenderOption";
 import Vector2 from "@/utils/Vector2";
 import TextWidget from "@/widget/TextWidget";
 import BackgroundWidget from "@/widget/BackgroundWidget";
+import SpriteWidget from "@/widget/SpriteWidget";
+import Sprite from "@/sprite/Sprite";
+import ReloadPng from "~/ui/button/restart.png";
 
 class MainActivity extends Activity {
     /** @param {App} app */
@@ -15,8 +18,14 @@ class MainActivity extends Activity {
             this.background.pos.x -= diffSecs / 10;
         };
         const titleText = new TextWidget(new Vector2(270, 40), "random race", RenderOption.absolute().scale(3));
+        const reloadButton = new SpriteWidget(new Vector2(430, 40), new Sprite(21, 22).setImage(ReloadPng), RenderOption.absolute().scale(2));
+        reloadButton.onClick = () => {
+            this.background.sprite = BackgroundSpriteSheet.random();
+            return true;
+        };
         this.addWidget(background);
         this.addWidget(titleText);
+        this.addWidget(reloadButton);
     }
 }
 
