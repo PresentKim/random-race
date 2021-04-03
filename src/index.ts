0049aa13c8fcdf97ea53b0595b84bc428907fb36
PresentKim <debe3721@gmail.com>
2021-04-04 05:34:46 +0900

    Support for more screen ratios (9:16 ~ 9:21)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import App from "@/App";
 import MainActivity from "@/activity/MainActivity";
 
+const canvas = document.createElement("canvas");
+canvas.id = "game";
+const app = new App(canvas);
+app.addActivity(new MainActivity(app));
+
+const widthMin = 1080 * 16 / 9; //ratio 9:16 (Galaxy 4~)
+const widthMax = 1080 * 21 / 9; //ratio 9:21 (~Galaxy Fold)
+function resizingCanvas() {
+    const beforeWidth = canvas.width;
+    canvas.width = Math.min(widthMax, Math.max(widthMin, window.innerWidth / window.innerHeight * 1080));
+    canvas.height = 1080;
+}
+
 //Init-game
 document.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.createElement("canvas");
-    canvas.width = 1024;
-    canvas.height = 576;
-    canvas.id = "game";
+    resizingCanvas();
     document.body.append(canvas);
 
-    const app = new App(canvas);
-    app.addActivity(new MainActivity(app));
     app.update();
 });
+
+window.addEventListener("resize", resizingCanvas);
+window.addEventListener("orientationchange", resizingCanvas);
