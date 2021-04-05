39a475dd5105b199a0a94d63fb768005fe38d1b5
PresentKim <debe3721@gmail.com>
2021-04-05 13:37:45 +0900

    Prevent scale up

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
             title: "Random Race",
             favicon: "./assets/favicon/favicon.ico",
             meta: {
-                viewport: "width=device-width, initial-scale=1",
+                viewport: "width=device-width, initial-scale=1, user-scalable=no",
                 description: "Random race game in which several characters run automatically"
             }
         }),
