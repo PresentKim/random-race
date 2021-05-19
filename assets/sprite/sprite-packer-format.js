b9d5e570784a4a2c2deee9f91aa505f1c09c180c
PresentKim <debe3721@gmail.com>
2021-05-19 21:24:27 +0900

    refactor : Load sprites in group units and Use custom pivot and animation fps data
    Load sprites in group units for increase performance and file management efficiency.
    
    SpriteSheetPacker does not support pivot point and animation data.
    Therefore written json separately and then using it.

--- assets/sprite/sprite-packer-format.js
+++ assets/sprite/sprite-packer-format.js
@@ -7,37 +7,38 @@
  * @param {{frame: {}, sourceColorRect:{}, sourceSize:{}}} spriteFrames
  */
 function exportSpriteSheet(dataFilePath, _, spriteFrames) {
-    var frames = {};
+    var sheets = {};
     for (var key in spriteFrames) {
         if (!spriteFrames.hasOwnProperty(key))
             continue;
 
         //Remove file extension and directory path
-        var spriteName = key.replace(/^.*[\\\/]/, '').replace(/\.[^/.]+$/, '');
-        if (spriteName.startsWith("_"))
+        var matches = key.match(/[a-z0-9\-_]+[\\\/]([^_][a-z0-9\-_]+)[\\\/]([^_][a-z0-9\-_]+)$/i);
+        if (!matches)
             continue;
 
+        var sheetName = matches[1];
+        var spriteName = matches[2];
+
         //Put sprite data
-        var frame = {};
-        frame.sw = spriteFrames[key].frame.width; //source width
-        frame.sh = spriteFrames[key].frame.height; //source height
-        frame.sx = spriteFrames[key].frame.x; //source x
-        frame.sy = spriteFrames[key].frame.y; //source y
+        var frames = sheets[sheetName] || {};
+        frames[spriteName] = {
+            sw: spriteFrames[key].frame.width, //source width
+            sh: spriteFrames[key].frame.height, //source height
+            sx: spriteFrames[key].frame.x, //source x
+            sy: spriteFrames[key].frame.y, //source y
 
-        frame.tx = spriteFrames[key].sourceColorRect.x; //trimmed x
-        frame.ty = spriteFrames[key].sourceColorRect.y; //trimmed y
+            tx: spriteFrames[key].sourceColorRect.x, //trimmed x
+            ty: spriteFrames[key].sourceColorRect.y, //trimmed y
 
-        frame.ow = spriteFrames[key].sourceSize.width; //origin width
-        frame.oh = spriteFrames[key].sourceSize.height; //origin height
-
-        frame.px = frame.ow / 2; //pivot x (center)
-        frame.py = frame.oh / 2; //pivot y (center)
-
-        frames[spriteName] = frame;
+            ow: spriteFrames[key].sourceSize.width, //origin width
+            oh: spriteFrames[key].sourceSize.height //origin height
+        };
+        sheets[sheetName] = frames;
     }
 
     return {
-        data: JSON.stringify(frames, null, 0),
+        data: JSON.stringify(sheets, null, '\t'),
         format: "json"
     };
 }
--- src/sprite/SpriteManager.ts
+++ src/sprite/SpriteManager.ts
@@ -4,26 +4,35 @@ import SpriteSheet from "@/sprite/SpriteSheet";
 import {IPointData} from "@/utils/Vector2";
 import RandomMap from "@/utils/RandomMap";
 
+export declare interface GroupDataset {
+    [sheetName: string]: {
+        [spriteName: string]: SpriteData
+    }
+}
+
+export declare interface CustomData {
+    pivot?: Array<number>,
+    animation_fps?: {
+        [animation_name: string]: number;
+    }
+}
+
+export declare interface CustomDataset {
+    [regex: string]: CustomData
+}
+
+const DEFAULT_CUSTOM_DATA: CustomData = {
+    pivot: [0.5, 1],
+    animation_fps: {}
+}
+
 class SpriteManager {
-    public readonly sprites: RandomMap<string, Sprite>;
     public readonly sheets: RandomMap<string, SpriteSheet>;
     public readonly sheetGroups: RandomMap<string, RandomMap<string, SpriteSheet>>;
-    public readonly customPivots: RandomMap<string, Map<string, IPointData>>;
 
     constructor() {
-        this.sprites = new RandomMap<string, Sprite>();
         this.sheets = new RandomMap<string, SpriteSheet>();
         this.sheetGroups = new RandomMap<string, RandomMap<string, SpriteSheet>>();
-        this.customPivots = new RandomMap<string, Map<string, IPointData>>();
-    }
-
-    getSprite(spriteName: string): Sprite | null {
-        return this.sprites.get(spriteName) ?? null;
-    }
-
-    setSprite(spriteName: string, sprite: Sprite): this {
-        this.sprites.set(spriteName, sprite);
-        return this;
     }
 
     getSheet(sheetName: string): SpriteSheet | null {
@@ -44,33 +53,49 @@ class SpriteManager {
         return this;
     }
 
-    loadSheet(sheetName: string, image: ImageLike, dataset: Map<string, SpriteData>): this {
-        const sheet = new SpriteSheet();
-        dataset.forEach((spriteData, spriteName) => {
-            const sprite = new Sprite(spriteData, image);
+    loadSheetGroup(groupName: string, image: ImageLike, groupDataset: GroupDataset, customDataset: CustomDataset): this {
+        const sheetGroup = new RandomMap<string, SpriteSheet>();
+        let i = 0;
+        Object.entries(groupDataset).forEach(([sheetName, sheetDataset]) => {
+            const fullSheetName = `${groupName}/${sheetName}`;
 
-            this.setSprite(`${sheetName}/${spriteName}`, sprite);
-            sheet.sprites.set(spriteName, sprite);
-
-            const match = spriteName.match(/[_\-][\d]+$/i);
-            if (match) {
-                const animationName = spriteName.slice(0, spriteName.length - match[0].length);
-                if (!sheet.animations.has(animationName)) {
-                    sheet.animations.set(animationName, new SpriteAnimation());
+            //Prepare custom data (pivot value, animation fps, ETC...)
+            let customData: CustomData = DEFAULT_CUSTOM_DATA;
+            Object.entries(customDataset).forEach(([regex, data]) => {
+                if (fullSheetName.search(regex) !== -1) {
+                    customData = {...customData, ...data};
                 }
-                sheet.animations.get(animationName).frames.push(sprite);
-            }
+            });
+
+            //Create sprite sheet instance via sheet dataset and custom data
+            const sheet = new SpriteSheet();
+            Object.entries(sheetDataset).forEach(([spriteName, spriteData]) => {
+                spriteData.px = spriteData.ow * customData.pivot[0];
+                spriteData.py = spriteData.oh * customData.pivot[1];
+                const sprite = new Sprite(spriteData, image);
+
+                const match = spriteName.match(/[_\-][\d]+$/i);
+                if (match) { //if is this animation sprites, register to animation map
+                    const animationName = spriteName.slice(0, spriteName.length - match[0].length);
+                    if (!sheet.animations.has(animationName)) {
+                        sheet.animations.set(animationName, new SpriteAnimation());
+
+                        if (customData?.animation_fps?.hasOwnProperty(animationName)) {
+                            sheet.animations.get(animationName).setFps(customData?.animation_fps[animationName]);
+                        }
+                    }
+                    sheet.animations.get(animationName).frames.push(sprite);
+                } else {
+                    sheet.sprites.set(spriteName, sprite);
+                }
+            });
+
+            sheetGroup.set(sheetName, sheet);
+            this.setSheet(fullSheetName, sheet);
+
+            console.log(`%c│      └────[${++i}/${Object.keys(groupDataset).length}] : ${groupName}/%c${sheetName}`, "color: gray", "color: lightgray");
         });
-
-        this.setSheet(sheetName, sheet);
-
-        const match = sheetName.match(/([a-z]+)\/(.+)/i);
-        if (match) {
-            if (!this.sheetGroups.has(match[1])) {
-                this.sheetGroups.set(match[1], new RandomMap<string, SpriteSheet>());
-            }
-            this.sheetGroups.get(match[1]).set(match[2], sheet)
-        }
+        this.setGroup(groupName, sheetGroup);
         return this;
     }
 }
